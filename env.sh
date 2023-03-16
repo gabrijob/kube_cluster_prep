6cae2c0605450be2d54dab9175a7316910aefe3d
Mariam <mariam@lyon>
2023-03-16 17:41:56 +0100

    update env

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-export DIR=/home/$USER && export PROM_NODE=$(tail -1 $DIR/nodes.txt |  cut -d "." -f 1)
+export DIR=/home/$USER && \
+export OAR_NODE_FILE=$DIR/nodes.txt && \
+export PROM_NODE=$(tail -1 $OAR_NODE_FILE |  cut -d "." -f 1)
