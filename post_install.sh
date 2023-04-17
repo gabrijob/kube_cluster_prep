d651f4589612d59885feb343094515fe3e37904e
Gabriel J.A.G <gabrijob@gmail.com>
2023-04-17 10:49:51 +0200

    added limit ranges default configs

--- post_install.sh
+++ post_install.sh
@@ -18,6 +18,7 @@ for i in $(cat ${filename}); do
 	#ssh-copy-id $USER@$i
 	ssh root@$i 'bash -s' < setups/post_install_setup.sh
 	scp kubeadm-config.yaml root@$i: 
+	scp conf/limitrange/big-limit-range.yaml root@$i:
 	ssh root@$i 'swapoff -a'
 	ssh root@$i 'bash -s' < setups/dataset_gen_setup.sh
 #	ssh root@$i 'kubeadm init --config kubeadm-config.yaml'
