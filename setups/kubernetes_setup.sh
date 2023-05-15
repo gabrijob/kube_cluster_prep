1b830a8c0bc49585fcbc8e462bc96de7cea4ca7d
Mariam <mariam@lyon>
2023-05-15 09:10:06 +0200

    fix kubelete version

--- setups/kubernetes_setup.sh
+++ setups/kubernetes_setup.sh
@@ -7,5 +7,5 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=1.26.2-00 kubeadm=1.26.2-00 kubectl=1.26.2-00
 sudo apt-mark hold kubelet kubeadm kubectl
