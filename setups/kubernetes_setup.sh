1fd92837e23a26b6f8bb99967493940946f99ef1
Mariam <mariam@lyon>
2023-05-30 18:12:13 +0200

    kube and prom fix

--- setups/kubernetes_setup.sh
+++ setups/kubernetes_setup.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl  -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb [trusted=yes] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 sudo apt-get install -y kubelet=1.26.2-00 kubeadm=1.26.2-00 kubectl=1.26.2-00
--- setups/setup_monitoring.sh
+++ setups/setup_monitoring.sh
@@ -25,7 +25,7 @@ kubectl create namespace monitoring
 ##### Setup the Prometheus helm chart #####
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm upgrade --install kube-prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --values $root_dir/conf/prometheus/kube-prometheus-stack-values.yaml
+helm upgrade --install kube-prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --values $root_dir/conf/prometheus/kube-prometheus-stack-values.yaml  --no-hooks
 
 
 #### ####
